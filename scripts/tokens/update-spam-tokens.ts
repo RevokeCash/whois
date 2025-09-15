6db673523ae2e644bdefdbe82fd05325ab851b78
Rosco Kalis <roscokalis@gmail.com>
2025-09-15 16:24:07 +0200

    Move to Covalent's spam list + small updates

--- scripts/tokens/update-spam-tokens.ts
+++ scripts/tokens/update-spam-tokens.ts
@@ -1,36 +1,49 @@
-import { ChainId } from '@revoke.cash/chains';
-import { Address } from 'viem';
 import { writeData } from '../utils';
-import { ALCHEMY_API_KEY } from '../utils/constants';
+import path from 'path';
+import { execSync } from 'child_process';
+import { existsSync, readFileSync } from 'fs';
+import walkdir from 'walkdir';
+import yaml from 'yaml';
 
-const CHAIN_URLS = {
-  [ChainId.EthereumMainnet]: `https://eth-mainnet.g.alchemy.com/nft/v3/${ALCHEMY_API_KEY}/getSpamContracts`,
-  [ChainId.PolygonMainnet]: `https://polygon-mainnet.g.alchemy.com/nft/v3/${ALCHEMY_API_KEY}/getSpamContracts`,
-  // [ChainId.ArbitrumOne]: `https://arb-mainnet.g.alchemy.com/nft/v3/${ALCHEMY_API_KEY}/getSpamContracts`,
-  // [ChainId.OPMainnet]: `https://opt-mainnet.g.alchemy.com/nft/v3/${ALCHEMY_API_KEY}/getSpamContracts`,
-  // [ChainId.Base]: `https://base-mainnet.g.alchemy.com/nft/v3/${ALCHEMY_API_KEY}/getSpamContracts`,
-};
+const CLONE_REPO = 'git@github.com:covalenthq/goldrush-enhanced-spam-lists.git';
+const CLONE_PATH = path.join(__dirname, '..', '..', 'temp', 'goldrush-enhanced-spam-lists');
+const LISTS_PATH = path.join(CLONE_PATH, 'src', 'lists');
 
 const updateSpamTokens = async () => {
-  console.log('Updating spam tokens');
-
-  for (const chainId of Object.keys(CHAIN_URLS)) {
-    await updateSpamTokensForChain(Number(chainId));
-  }
+  await importFromCovalent();
 };
 
-const updateSpamTokensForChain = async (chainId: number) => {
-  const res = await fetch(CHAIN_URLS[chainId]);
+const importFromCovalent = async (): Promise<void> => {
+  console.log('Updating covalent github spam tokens');
 
-  if (!res.ok) {
-    throw new Error(`Failed to fetch spam tokens for chain ${chainId}: ${await res.text()}`);
+  if (existsSync(CLONE_PATH)) {
+    execSync(`pushd ${CLONE_PATH} && git pull && popd`);
+    console.log('GitHub repository pulled.');
+  } else {
+    execSync(`git clone ${CLONE_REPO} ${CLONE_PATH}`);
+    console.log('GitHub repository cloned.');
   }
 
-  const { contractAddresses: spamTokens }: { contractAddresses: Address[] } = await res.json();
+  await processSpamTokenFiles();
+  console.log('Spam token files written.');
 
-  await Promise.all(
-    spamTokens.map((address) => writeData('generated', 'tokens', String(chainId), address, { isSpam: true })),
-  );
+  execSync(`rm -rf ${CLONE_PATH}`);
+  console.log('GitHub repository removed.');
 };
 
-// updateSpamTokens();
+const processSpamTokenFiles = async () => {
+  const paths = walkdir.sync(LISTS_PATH).filter((filePath) => filePath.endsWith('.yaml') && !filePath.includes('maybe'));
+
+  for (const filePath of paths) {
+    console.log(`Processing ${filePath}`);
+    const spamContracts = yaml.parse(readFileSync(filePath, 'utf-8')).SpamContracts;
+    await Promise.all(spamContracts.map(async (spamContract: string) => {
+      const [chainId, address, confidence] = spamContract.split('/');
+      if (parseInt(confidence) < 20) return;
+      return writeData('generated', 'tokens', chainId, address, { isSpam: true });
+    }));
+  }
+
+};
+
+updateSpamTokens();
